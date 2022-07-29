aa959d8d2441d4035884da7b3964ff88cdf49b5d
Ufuk ORHAN <ufuk.orhan@logsign.net>
2022-07-29 20:47:37 +0300

    feat: password is hashed

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
-from typing import  List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-import psycopg2
-from psycopg2.extras import RealDictCursor
 import time
+from typing import List
+
+import psycopg2
+from fastapi import Depends, FastAPI, HTTPException, Response, status
+from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
-from . import models, schemas
-from .database import  engine, get_db
+
+from . import models, schemas, utils
+from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -39,7 +41,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
-    new_post = models.Post(**post.dict())  # title=post.title, content=post.content, published=post.published ayni anlamda ancak modele birsey ekledigimizde otomatik unpacked edicek bu yontem daha!
+    new_post = models.Post(**post.dict())  
     db.add(new_post)
     db.commit()
     db.refresh(new_post)   # the same purpose RETURNING * 
@@ -86,7 +88,11 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    new_user = models.User(**user.dict())  # title=post.title, content=post.content, published=post.published ayni anlamda ancak modele birsey ekledigimizde otomatik unpacked edicek bu yontem daha!
+    # hast the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
+    new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)   # the same purpose RETURNING * 
