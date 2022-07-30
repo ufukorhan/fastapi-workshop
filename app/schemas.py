83e2d6e405ea6e425beb03ccace437642206fc26
Ufuk ORHAN <ufuk.orhan@logsign.net>
2022-07-30 17:47:14 +0300

    feat: add jwt token operations and verify user is logged in

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 class PostBase(BaseModel): # Request Model
     title: str
@@ -28,3 +29,14 @@ class UserOut(BaseModel): # Response Model
 
     class Config:
         orm_mode = True
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
