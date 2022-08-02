354c1cf53dc80ce825b30a2aca4008c709f38f83
Ufuk ORHAN <ufuk.orhan@logsign.net>
2022-08-02 21:07:25 +0300

    feat: add corsmiddleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from . import models
-from .database import engine
+
 from .routers import auth, post, user, vote
 
 # instead of this i am using alembic
@@ -10,6 +10,16 @@ from .routers import auth, post, user, vote
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, # A list of origins that should be permitted to make cross-origin requests.
+    # E.g. ['https://example.org', 'https://www.example.org']. We can use ['*'] to allow any origin.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
